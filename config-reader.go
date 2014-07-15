ca2be4d84001a37c5f1b6680341e073e1f08e19f
metacoin <support@colorcoin.org>
2014-07-15 12:58:45 -0400

    added configuration file

---
+++ config-reader.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func ReadConfig(fileName string) {
+
+	type Configuration struct {
+		Users  []string
+		Groups []string
+	}
+
+	file, _ := os.Open("conf.json")
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	fmt.Println(configuration.Users)
+}
---
+++ multicurrency-payment-system.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// initialize gorilla mux
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("{method:[a-z]+}", MethodHandler).Methods("GET")
+	http.Handle("/", rtr)
+
+	// start listening on port specified in config
+	log.Println("Listening...")
+	err := http.ListenAndServe(":3000", nil)
+
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+		fmt.Printf("ListenAndServe:%s\n", err.Error())
+	}
+}
+
+func MethodHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("success")
+}
