5ddf816cc60726b4c95024ae38f8a2c95c9d23ba
metacoin <support@colorcoin.org>
2014-07-17 19:50:50 -0400

    refactored cryptsy log function

--- cryptsy-quote.go
+++ cryptsy-quote.go
@@ -50,11 +50,11 @@ func CryptsyGetMarket(n string) {
 	}
 
 	// testing output
-	CryptsyLog(bodyString)
+	CryptsyTestLog(bodyString)
 
 }
 
-func CryptsyLog(textToLog string) {
+func CryptsyTestLog(textToLog string) {
 	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("error opening file: %v", err)
