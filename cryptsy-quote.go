637349ce5d8bcdef9be8f819ff4c42f75f20aa57
metacoin <support@colorcoin.org>
2014-07-17 19:47:25 -0400

    added json test file output

--- cryptsy-quote.go
+++ cryptsy-quote.go
@@ -57,7 +57,7 @@ func CryptsyGetMarket(n string) {
 func CryptsyLog(textToLog string) {
 	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		t.Fatalf("error opening file: %v", err)
+		log.Fatal("error opening file: %v", err)
 	}
 	defer f.Close()
 
